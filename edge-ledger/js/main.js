d5f7c04007e955b9fd48f5a204f2cef0e39bf534
gajendra <gajendra.wbb.com>
2020-07-12 22:20:09 +0530

    build a sample site using flex-box

---
+++ edge-ledger/js/main.js
@@ -0,0 +1,36 @@
+// Initialise and add map
+function initMap() {
+  // The location
+  var crp = {lat: 12.9035, lng: 76.3898};
+  // The map, centered at location mentioned
+  var map = new google.maps.Map(
+      document.querySelector('.map'), {zoom: 4, center: crp});
+  // The marker, positioned at location mentione
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+// Sticky Menu Background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+})
+
+
+// Smooth scroll navbar
+$('#navbar a, a.btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
